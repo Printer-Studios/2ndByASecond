3fdf2c09395d5f80b855029e55606a659c811f5f
ElBuenTocino <paumm8@gmail.com>
2026-01-13 13:14:10 +0100

    canvi de coses per web
    en teoria mig arreglat

--- Assets/Scripts/SpriteManager.cs
+++ Assets/Scripts/SpriteManager.cs
@@ -3,18 +3,48 @@ using UnityEngine;
 
 public class SpriteManager : MonoBehaviour
 {
-    public List<Sprite> sprites;
-    public GameObject playerCars;
-    public GameObject[] cars;
-    
+    [Header("Sprites")]
+    [SerializeField] private List<Sprite> sprites;
+
+    [Header("Cars")]
+    [SerializeField] private GameObject[] cars;
+
     void Start()
     {
-        //cars[0] = (playerCars.transform.GetChild(PlayerPrefs.GetInt("PlayerSprite")).gameObject);
-        sprites.RemoveAt(PlayerPrefs.GetInt("PlayerSprite"));
-        for (int i = 0; i < sprites.Count; i++)
+        // Get saved player sprite index (default to 0)
+        int playerSpriteIndex = PlayerPrefs.GetInt("PlayerSprite", 0);
+
+        // Make a safe copy so we don't permanently modify the Inspector list
+        List<Sprite> availableSprites = new List<Sprite>(sprites);
+
+        // Validate index before removing
+        if (playerSpriteIndex >= 0 && playerSpriteIndex < availableSprites.Count)
         {
-            cars[i].GetComponent<SpriteRenderer>().sprite = sprites[i];
+            availableSprites.RemoveAt(playerSpriteIndex);
+        }
+        else
+        {
+            Debug.LogError(
+                $"PlayerSprite index {playerSpriteIndex} is out of range. " +
+                $"Sprites count: {availableSprites.Count}"
+            );
+        }
+
+        // Assign sprites to cars safely
+        int count = Mathf.Min(availableSprites.Count, cars.Length);
+
+        for (int i = 0; i < count; i++)
+        {
+            SpriteRenderer sr = cars[i].GetComponent<SpriteRenderer>();
+            if (sr != null)
+            {
+                sr.sprite = availableSprites[i];
+            }
+            else
+            {
+                Debug.LogWarning($"Car at index {i} has no SpriteRenderer.");
+            }
         }
     }
-    
 }
+
