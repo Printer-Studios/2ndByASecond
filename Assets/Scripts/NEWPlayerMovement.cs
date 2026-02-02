47b71f718fcdce0f5024b0ce8c11638631826dc4
ElBuenTocino <paumm8@gmail.com>
2026-02-02 12:24:38 +0100

    New player movement new input system

--- Assets/Scripts/NEWPlayerMovement.cs
+++ Assets/Scripts/NEWPlayerMovement.cs
@@ -5,29 +5,31 @@ using UnityEngine.PlayerLoop;
 public class NEWPlayerMovement : Movement
 {
     public float increaseSpeed;
-    public InputActionReference upAction;
-    public InputActionReference leftAction;
-    public InputActionReference rightAction;
-    public InputActionReference boostAction;
+    private Vector2 moveInput;
+    private float lastX;
 
     void Update()
     {
         base.Update();
-        if (upAction.action.IsPressed())
+
+        if (moveInput.y > 0.5f) // UP
         {
             speed += increaseSpeed * Time.deltaTime;
         }
-        if (leftAction.action.WasPressedThisFrame())
-        {
-            ChangeLane(-1);
-        }
-        if (rightAction.action.WasPressedThisFrame())
-        {
-            ChangeLane(1);
-        }
-        if (boostAction.action.WasPressedThisFrame())
-        {
-            //
-        }
     }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+
+        if (input.x > 0.5f && lastX <= 0.5f) // RIGHT
+            ChangeLane(1);
+
+        if (input.x < -0.5f && lastX >= -0.5f) // LEFT
+            ChangeLane(-1);
+
+        lastX = input.x;
+        moveInput = input;
+    }
+
 }
